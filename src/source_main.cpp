30830e123d6c453dec0b5df5dc913540e2ca198b
Andreas Longva <andreas.b.longva@gmail.com>
2015-10-10 15:40:56 +0200

    Minimal working example for directly piping audio over TCP

--- src/source_main.cpp
+++ src/source_main.cpp
@@ -1,4 +1,29 @@
+#include <boost/asio.hpp>
+#include <array>
+#include <iostream>
+
+namespace asio = boost::asio;
+
+using boost::asio::ip::tcp;
+
 int main()
 {
+    asio::io_service io_service;
+    tcp::socket socket(io_service);
+    tcp::endpoint endpoint( asio::ip::address::from_string("127.0.0.1"), 1582);
+    socket.connect(endpoint);
+
+    std::cout << "Connected successfully to endpoint." << std::endl;
+
+    std::array<char, 8192> buffer;
+
+    while(true)
+    {
+        std::cin.read(buffer.data(), 8192);
+        auto readcount = std::cin.gcount();
+
+        boost::system::error_code error_code;
+        socket.write_some(asio::buffer(buffer, readcount), error_code);
+    }
 
 }
--- src/speaker_main.cpp
+++ src/speaker_main.cpp
@@ -1,4 +1,30 @@
+#include <boost/asio.hpp>
+#include <array>
+#include <iostream>
+
+namespace asio = boost::asio;
+
+using boost::asio::ip::tcp;
+
 int main()
 {
+    asio::io_service io_service;
+    tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 1582));
+
+    std::array<char, 8192> buffer;
+
+    // Accept only a single connection at a time
+    while (true)
+    {
+        tcp::socket socket(io_service);
+        acceptor.accept(socket);
+        boost::system::error_code error_code;
+
+        while (error_code != boost::asio::error::eof) {
+            size_t len = socket.read_some(asio::buffer(buffer), error_code);
+            std::cout.write(buffer.data(), len);
+        }
+    }
+
 
 }
